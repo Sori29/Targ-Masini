6a03a9709512528d61c15d2cf2cffd16ad4f23db
Sori29 <sorin.lungu292002@gmail.com>
2022-04-28 17:43:19 +0300

    Implementare interfata de baza pentru adaugarea unei masini in fisier prin UI, fara validarea informatilor

--- UI_MainMenu/Form1.cs
+++ UI_MainMenu/Form1.cs
@@ -16,7 +16,10 @@ namespace UI_MainMenu
 {
     public partial class Form1 : Form
     {
-        AdministareMasini_FisierTxt adminMasini;
+        static string numeFisier = ConfigurationManager.AppSettings["NumeFisier"];
+        static string locatieFisierSolutie = Directory.GetParent(System.IO.Directory.GetCurrentDirectory()).Parent.Parent.FullName;
+        static string caleCompletaFisier = locatieFisierSolutie + "\\" + numeFisier;
+        AdministareMasini_FisierTxt adminMasini = new AdministareMasini_FisierTxt(caleCompletaFisier);
 
         private Label lblNumeFirma;
         private Label lblNumeModel;
@@ -36,9 +39,11 @@ namespace UI_MainMenu
         private Label lblnCuloare;
         private Label lblnOptiuni;
 
-        private TextBox txtNume;
-        private TextBox txtPrenume;
-        private TextBox txtNote;
+        private TextBox txtNumeFirma;
+        private TextBox txtNumeModel;
+        private TextBox txtAn;
+        private TextBox txtCuloare;
+        private TextBox txtOptiuni;
 
         private Button btnAdauga;
         private Button btnRefresh;
@@ -48,19 +53,16 @@ namespace UI_MainMenu
         private const int LATIME_CONTROL = 100;
         private const int DIMENSIUNE_PAS_Y = 30;
         private const int DIMENSIUNE_PAS_X = 100;
+        private const int POZITIE_START_X = 50;
         public Form1()
         {
-            string numeFisier = ConfigurationManager.AppSettings["NumeFisier"];
-            string locatieFisierSolutie = Directory.GetParent(System.IO.Directory.GetCurrentDirectory()).Parent.Parent.FullName;
-            string caleCompletaFisier = locatieFisierSolutie + "\\" + numeFisier;
-            AdministareMasini_FisierTxt adminMasini = new AdministareMasini_FisierTxt(caleCompletaFisier);
             InitializeComponent();
             Masina[] masini = adminMasini.GetMasini(out int nrMasini);
 
             //setare proprietati
-            this.Size = new Size(1000, 300);
+            this.Size = new Size(1200, 400);
             this.StartPosition = FormStartPosition.Manual;
-            this.Location = new Point(100, 100);
+            this.Location = new Point(0, 100);
             this.Font = new Font("Arial", 9, FontStyle.Bold);
             this.ForeColor = Color.LimeGreen;
             this.Text = "Informatii masini";
@@ -69,7 +71,8 @@ namespace UI_MainMenu
             lblNumeFirma = new Label();
             lblNumeFirma.Width = LATIME_CONTROL;
             lblNumeFirma.Text = "Nume firma";
-            lblNumeFirma.Left = DIMENSIUNE_PAS_X;
+            lblNumeFirma.Left = POZITIE_START_X;
+            lblNumeFirma.Top= 4 * DIMENSIUNE_PAS_Y;
             lblNumeFirma.ForeColor = Color.DarkGreen;
             this.Controls.Add(lblNumeFirma);
 
@@ -77,7 +80,8 @@ namespace UI_MainMenu
             lblNumeModel = new Label();
             lblNumeModel.Width = LATIME_CONTROL;
             lblNumeModel.Text = "Nume model";
-            lblNumeModel.Left = 2 * DIMENSIUNE_PAS_X;
+            lblNumeModel.Left = 2 * DIMENSIUNE_PAS_X + POZITIE_START_X;
+            lblNumeModel.Top = 4 * DIMENSIUNE_PAS_Y;
             lblNumeModel.ForeColor = Color.DarkGreen;
             this.Controls.Add(lblNumeModel);
 
@@ -85,7 +89,8 @@ namespace UI_MainMenu
             lblAn = new Label();
             lblAn.Width = LATIME_CONTROL;
             lblAn.Text = "An fabricatie";
-            lblAn.Left = 3 * DIMENSIUNE_PAS_X;
+            lblAn.Left = 4 * DIMENSIUNE_PAS_X + 50;
+            lblAn.Top = 4 * DIMENSIUNE_PAS_Y;
             lblAn.ForeColor = Color.DarkGreen;
             this.Controls.Add(lblAn);
 
@@ -93,7 +98,8 @@ namespace UI_MainMenu
             lblCuloare = new Label();
             lblCuloare.Width = LATIME_CONTROL;
             lblCuloare.Text = "Culoare";
-            lblCuloare.Left = 4 * DIMENSIUNE_PAS_X;
+            lblCuloare.Left = 6 * DIMENSIUNE_PAS_X + POZITIE_START_X;
+            lblCuloare.Top = 4 * DIMENSIUNE_PAS_Y;
             lblCuloare.ForeColor = Color.DarkGreen;
             this.Controls.Add(lblCuloare);
 
@@ -101,17 +107,90 @@ namespace UI_MainMenu
             lblOptiuni = new Label();
             lblOptiuni.Width = LATIME_CONTROL;
             lblOptiuni.Text = "Optiuni";
-            lblOptiuni.Left = 5 * DIMENSIUNE_PAS_X;
+            lblOptiuni.Left = 8 * DIMENSIUNE_PAS_X + POZITIE_START_X;
+            lblOptiuni.Top = 4 * DIMENSIUNE_PAS_Y;
             lblOptiuni.ForeColor = Color.DarkGreen;
             this.Controls.Add(lblOptiuni);
 
+            //adaugare contropl de tip button pentru 'Refresh';
             btnRefresh = new Button();
             btnRefresh.Width = LATIME_CONTROL;
-            btnRefresh.Left = 7 * DIMENSIUNE_PAS_X;
+            btnRefresh.Left = 10 * DIMENSIUNE_PAS_X + POZITIE_START_X;
+            btnRefresh.Top = DIMENSIUNE_PAS_Y;
             btnRefresh.Text = "Refresh";
             btnRefresh.Click += OnButtonClicked;
             this.Controls.Add(btnRefresh);
+
+            //adaugare control de tip button pentru 'Adaugare masina'
+            btnAdauga = new Button();
+            btnAdauga.Width = LATIME_CONTROL;
+            btnAdauga.Location = new System.Drawing.Point(10 * DIMENSIUNE_PAS_X + POZITIE_START_X, 2 * DIMENSIUNE_PAS_Y);
+            btnAdauga.Text = "Adauga";
+            btnAdauga.Click += OnButton1Clicked;
+            this.Controls.Add(btnAdauga);
+
             //adaugare control de tip Label pentru 'Introducere date noi';
+            lblnNumeFirma = new Label();
+            lblnNumeFirma.Width = 2 * LATIME_CONTROL;
+            lblnNumeFirma.Text = "Introduceti firma";
+            lblnNumeFirma.Left = POZITIE_START_X;
+            lblnNumeFirma.ForeColor = Color.DarkGreen;
+            this.Controls.Add(lblnNumeFirma);
+
+            lblnNumeModel = new Label();
+            lblnNumeModel.Width = 2 * LATIME_CONTROL;
+            lblnNumeModel.Text = "Introduceti model";
+            lblnNumeModel.Left = 2 * DIMENSIUNE_PAS_X + POZITIE_START_X;
+            lblnNumeModel.ForeColor = Color.DarkGreen;
+            this.Controls.Add(lblnNumeModel);
+
+            lblnAn = new Label();
+            lblnAn.Width = 2 * LATIME_CONTROL;
+            lblnAn.Text = "Introduceti an fabricatie";
+            lblnAn.Left = 4 * DIMENSIUNE_PAS_X + POZITIE_START_X;
+            lblnAn.ForeColor = Color.DarkGreen;
+            this.Controls.Add(lblnAn);
+
+            lblnCuloare = new Label();
+            lblnCuloare.Width = 2 * LATIME_CONTROL;
+            lblnCuloare.Text = "Introduceti culoare";
+            lblnCuloare.Left = 6 * DIMENSIUNE_PAS_X + POZITIE_START_X;
+            lblnCuloare.ForeColor = Color.DarkGreen;
+            this.Controls.Add(lblnCuloare);
+
+            lblnOptiuni = new Label();
+            lblnOptiuni.Width = 2 * LATIME_CONTROL;
+            lblnOptiuni.Text = "Introduceti optiuni";
+            lblnOptiuni.Left = 8 * DIMENSIUNE_PAS_X + POZITIE_START_X;
+            lblnOptiuni.ForeColor = Color.DarkGreen;
+            this.Controls.Add(lblnOptiuni);
+
+            //introducere control de tip textbox pentru 'Introducere date noi';
+            txtNumeFirma = new TextBox();
+            txtNumeFirma.Width = LATIME_CONTROL;
+            txtNumeFirma.Location = new System.Drawing.Point(POZITIE_START_X, DIMENSIUNE_PAS_Y);
+            this.Controls.Add(txtNumeFirma);
+
+            txtNumeModel = new TextBox();
+            txtNumeModel.Width = LATIME_CONTROL;
+            txtNumeModel.Location = new System.Drawing.Point(POZITIE_START_X + 2 * DIMENSIUNE_PAS_X, DIMENSIUNE_PAS_Y);
+            this.Controls.Add(txtNumeModel);
+
+            txtAn = new TextBox();
+            txtAn.Width = LATIME_CONTROL;
+            txtAn.Location = new System.Drawing.Point(POZITIE_START_X + 4 * DIMENSIUNE_PAS_X, DIMENSIUNE_PAS_Y);
+            this.Controls.Add(txtAn);
+
+            txtCuloare = new TextBox();
+            txtCuloare.Width = LATIME_CONTROL;
+            txtCuloare.Location = new System.Drawing.Point(POZITIE_START_X + 6 * DIMENSIUNE_PAS_X, DIMENSIUNE_PAS_Y);
+            this.Controls.Add(txtCuloare);
+
+            txtOptiuni = new TextBox();
+            txtOptiuni.Width = LATIME_CONTROL;
+            txtOptiuni.Location = new System.Drawing.Point(POZITIE_START_X + 8 * DIMENSIUNE_PAS_X, DIMENSIUNE_PAS_Y);
+            this.Controls.Add(txtOptiuni);
+
 
         }
         private void Form1_Load(object sender, EventArgs e)
@@ -123,12 +202,20 @@ namespace UI_MainMenu
         {
             AfiseazaMasini();
         }
+        private void OnButton1Clicked(object sender, EventArgs e)
+        {
+            Masina[] masini = adminMasini.GetMasini(out int nrMasini);
+            string firma = txtNumeFirma.Text;
+            string model = txtNumeModel.Text;
+            int an = Int32.Parse(txtAn.Text);
+            string culoare = txtCuloare.Text;
+            string optiuni = txtOptiuni.Text;
+
+            Masina masina = new Masina(nrMasini+1,firma,model,an,culoare,optiuni);
+            adminMasini.AddMasina(masina);
+        }
         private void AfiseazaMasini()
         {
-            string numeFisier = ConfigurationManager.AppSettings["NumeFisier"];
-            string locatieFisierSolutie = Directory.GetParent(System.IO.Directory.GetCurrentDirectory()).Parent.Parent.FullName;
-            string caleCompletaFisier = locatieFisierSolutie + "\\" + numeFisier;
-            AdministareMasini_FisierTxt adminMasini = new AdministareMasini_FisierTxt(caleCompletaFisier);
             Masina[] masini = adminMasini.GetMasini(out int nrMasini);
 
             lblsNumeFirma = new Label[nrMasini];
@@ -146,40 +233,40 @@ namespace UI_MainMenu
                 lblsNumeFirma[i] = new Label();
                 lblsNumeFirma[i].Width = LATIME_CONTROL;
                 lblsNumeFirma[i].Text = masina.numeFirma;
-                lblsNumeFirma[i].Left = DIMENSIUNE_PAS_X;
-                lblsNumeFirma[i].Top = (i + 1) * DIMENSIUNE_PAS_Y;
+                lblsNumeFirma[i].Left = 50;
+                lblsNumeFirma[i].Top = (i + 5) * DIMENSIUNE_PAS_Y;
                 this.Controls.Add(lblsNumeFirma[i]);
 
                 //adaugare control de tip Label pentru numele modelelor;
                 lblsNumeModel[i] = new Label();
                 lblsNumeModel[i].Width = LATIME_CONTROL;
                 lblsNumeModel[i].Text = masina.model;
-                lblsNumeModel[i].Left = 2 * DIMENSIUNE_PAS_X;
-                lblsNumeModel[i].Top = (i + 1) * DIMENSIUNE_PAS_Y;
+                lblsNumeModel[i].Left = 2 * DIMENSIUNE_PAS_X + 50;
+                lblsNumeModel[i].Top = (i + 5) * DIMENSIUNE_PAS_Y;
                 this.Controls.Add(lblsNumeModel[i]);
 
                 //adaugare control de tip Label pentru anul masinilor;
                 lblsAn[i] = new Label();
                 lblsAn[i].Width = LATIME_CONTROL;
                 lblsAn[i].Text = masina.an.ToString();
-                lblsAn[i].Left = 3 * DIMENSIUNE_PAS_X;
-                lblsAn[i].Top = (i + 1) * DIMENSIUNE_PAS_Y;
+                lblsAn[i].Left = 4 * DIMENSIUNE_PAS_X + 50;
+                lblsAn[i].Top = (i + 5) * DIMENSIUNE_PAS_Y;
                 this.Controls.Add(lblsAn[i]);
 
                 //adaugare control de tip Label pentru Culoare masinilor;
                 lblsCuloare[i] = new Label();
                 lblsCuloare[i].Width = LATIME_CONTROL;
                 lblsCuloare[i].Text = masina.culoare;
-                lblsCuloare[i].Left = 4 * DIMENSIUNE_PAS_X;
-                lblsCuloare[i].Top = (i + 1) * DIMENSIUNE_PAS_Y;
+                lblsCuloare[i].Left = 6 * DIMENSIUNE_PAS_X + 50;
+                lblsCuloare[i].Top = (i + 5) * DIMENSIUNE_PAS_Y;
                 this.Controls.Add(lblsCuloare[i]);
 
                 //adaugare control de tip Label pentru Optiunile masinilor;
                 lblsOptiuni[i] = new Label();
                 lblsOptiuni[i].Width = 5 * LATIME_CONTROL;
                 lblsOptiuni[i].Text = masina.optiuni;
-                lblsOptiuni[i].Left = 5 * DIMENSIUNE_PAS_X;
-                lblsOptiuni[i].Top = (i + 1) * DIMENSIUNE_PAS_Y;
+                lblsOptiuni[i].Left = 8 * DIMENSIUNE_PAS_X + 50;
+                lblsOptiuni[i].Top = (i + 5) * DIMENSIUNE_PAS_Y;
                 this.Controls.Add(lblsOptiuni[i]);
                 i++;
             }
